51cd5e4faf8579982bb284986782fb91ceb5e5b0
raghavsaravanan <raghavsaro2008@gmail.com>
2025-07-18 13:53:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from ultralytics import YOLO
+from utils import read_video, save_video
+def main():
 
-model = YOLO("yolov8x")
+    # Read Video
+    video_frames=read_video("input_videos/video_1.mp4")
 
-results = model.predict("input_videos/video_1.mp4", save=True)
-print (results)
-print("====================")
-for box in results[0].boxes:
-    print(box)
+    # Save Video
+    save_video(video_frames, "output_videos/output_video1.avi")
 
-    
-aa
+if __name__ == "__main__":
+    main()
+
